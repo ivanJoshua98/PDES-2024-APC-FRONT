70517288645ae24101d484d4fcdb16cf9d2d4a73
jaraJulieta <jarajuli13@gmail.com>
2024-09-19 01:25:27 -0300

    page login

--- frontend-apc/src/App.jsx
+++ frontend-apc/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import Home from './pages/Home';
 import { Routes, Route } from "react-router-dom"
 import SearchResult from './pages/SearchResult';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path='/search-result/:keyWords' element={<SearchResult/>} />
+         {/* <Route path="/" element={<Home/>} />
+                <Route path='/search-result/:keyWords' element={<SearchResult/>} */}
+         <Route path="/login" element={<Login />} />
       </Routes>
       
     </div>
