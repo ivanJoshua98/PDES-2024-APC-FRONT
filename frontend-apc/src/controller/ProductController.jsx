edb90ebdcb71a165217c29892c6d16634e20720d
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2024-10-03 22:27:50 -0300

    Se integra funcionalidad para ingresar a un producto

--- frontend-apc/src/controller/ProductController.jsx
+++ frontend-apc/src/controller/ProductController.jsx
@@ -3,5 +3,7 @@ import { notAuthenticatedApiClient } from "./BaseApi";
 
 const searchProductsByWords = (words) => notAuthenticatedApiClient.get('/products/search/'.concat(words));
 
+const getProductByIdFromML = (productId) => notAuthenticatedApiClient.get('/products/search/item/'.concat(productId));
 
-export default {searchProductsByWords}
+
+export default {searchProductsByWords, getProductByIdFromML};
